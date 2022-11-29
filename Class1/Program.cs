46b24f0473e9a482b7c5def65c31258f7b7e62b9
Ayxan23 <ayxan042223@gmail.com>
2022-11-29 15:17:15 +0400

    Class1

---
+++ Class1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Class1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Student("Ayxan", "Mustafayev", 1, 95, true);
+            
+            Student student2 = new Student("Nicat", "Aliyev", 3, 48, false);
+
+            
+            student1.FullName();
+            student2.FullName();
+            
+            student1.AllValues();
+            student2.AllValues();
+
+            student1.NextExam();
+            student2.NextExam();
+
+        }
+    }
+}
---
+++ Class1/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+        public int Group;
+        public int Point;
+        public bool IsGraduated;
+
+        public Student(string name, string surname, int group, int point, bool isgraduated) 
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isgraduated;
+        }
+
+
+        public void FullName()
+        {
+            Console.WriteLine(Name + " " + Surname);
+        }
+
+
+        public void AllValues()
+        {
+            string mezun = "";
+
+            if(IsGraduated)
+            {
+                mezun = "Mezun olub";
+            }
+            else
+            {
+                mezun = "Mezun olmayib";
+            }
+            Console.WriteLine(Name + " " + Surname + " " + Group + " " + Point + " " + IsGraduated + " " + mezun);
+        }
+
+
+        public void NextExam()
+        {
+            if (Point > 80)
+            {
+                Console.WriteLine("Novbeti imtahana gire biler");
+            }
+            else
+            {
+                Console.WriteLine("Novbeti imtahana gire bilmez");
+            }
+        }
+
+    }
+}
